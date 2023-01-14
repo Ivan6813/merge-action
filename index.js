a3b2016cf8e8c29b6228651dcad757e0076dc022
Линкевич Иван Владимирович <ilinkevich@alfaleasing.ru>
2023-01-14 12:45:07 +0300

    feat(*): create action

---
+++ index.js
@@ -0,0 +1,73 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const report = require('../../cypress/report/report.json');
+
+const main = async () => {
+    try {
+        const owner = core.getInput('owner', { required: true });
+        const repo = core.getInput('repo', { required: true });
+        const pull_number = core.getInput('pull_number', { required: true });
+        const token = core.getInput('token', { required: true });
+        const url = 'https://training.cleverland.by/pull-request/merged'; // /pull-request/opened
+        const minimum_required_result = 80;
+        const tests_pass_percent = report.stats.passPercent;
+        const total_tests = report.stats.tests;
+        const failures_test = report.stats.failures;
+        const issue_number = 123;
+        const body = `Процент пройденных: ${tests_pass_percent}. Всего тестов: ${total_tests}. Не пройдено: ${failures_test}.`;
+
+        const octokit = new github.getOctokit(token);
+
+        await octokit.rest.issues.createComment({
+            owner,
+            repo,
+            issue_number,
+            body,
+        });
+
+        const { html_url } = await octokit.rest.pulls.get({
+            owner,
+            repo,
+            pull_number,
+        });
+
+        if (tests_pass_percent >= minimum_required_result) {
+            await fetch(url, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify({ link: html_url, github: owner, isTestsSuccess: true })
+            });
+        } else {
+            await fetch(url, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify({ link: html_url, github: owner, isTestsSuccess: false })
+            });
+        }
+
+         // const { merged } = await octokit.rest.pulls.merge({
+        //     owner,
+        //     repo,
+        //     pull_number,
+        // });
+
+        // if (merged) {
+        //     await fetch(url, {
+        //         method: 'POST',
+        //         headers: {
+        //           'Content-Type': 'application/json;charset=utf-8'
+        //         },
+        //         body: JSON.stringify({ github: owner })
+        //     });
+        // }
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+// Call the main function to run the action
+main();
