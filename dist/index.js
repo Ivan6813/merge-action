9e9d1a2be8e797f4201ec52b748e5830b78f3d7b
Линкевич Иван Владимирович <ilinkevich@alfaleasing.ru>
2023-01-14 16:39:59 +0300

    com18

--- dist/index.js
+++ dist/index.js
@@ -9694,7 +9694,7 @@ const main = async () => {
         const repo = core.getInput('repo', { required: true });
         const pull_number = core.getInput('pull_number', { required: true });
         const token = core.getInput('token', { required: true });
-        // const url = 'https://training.cleverland.by/pull-request/merged'; // /pull-request/opened
+        const url = 'http://localhost:3021/pull-request/opened';
         // const minimum_required_result = 80;
         const tests_pass_percent = 50;
         // // const total_tests = report.stats.tests;
@@ -9717,6 +9717,16 @@ const main = async () => {
         });
 
         core.info(`url, ${JSON.stringify(data.html_url)}!!!`);
+
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({ link: data.html_url, github: owner, isTestsSuccess: true })
+        });
+
+        core.info(`url, ${JSON.stringify(response)}!!!`);
 
         // if (tests_pass_percent >= minimum_required_result) {
         //     await fetch(url, {
