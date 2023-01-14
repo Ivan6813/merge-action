964329e1f5e5b5397c64d0d924ef393ce970c572
Линкевич Иван Владимирович <ilinkevich@alfaleasing.ru>
2023-01-14 15:13:12 +0300

    commit 5

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const core = require('@actions/core');
-// const github = require('@actions/github');
+const github = require('@actions/github');
 // const report = require('../../cypress/report/report.json');
 
 const main = async () => {
     try {
         const owner = core.getInput('owner', { required: true });
-        // const repo = core.getInput('repo', { required: true });
-        // const pull_number = core.getInput('pull_number', { required: true });
-        // const token = core.getInput('token', { required: true });
+        const repo = core.getInput('repo', { required: true });
+        const pull_number = core.getInput('pull_number', { required: true });
+        const token = core.getInput('token', { required: true });
         // const url = 'https://training.cleverland.by/pull-request/merged'; // /pull-request/opened
         // const minimum_required_result = 80;
         // const tests_pass_percent = 50;
@@ -17,8 +17,11 @@ const main = async () => {
         // const body = `Процент пройденных: ${tests_pass_percent}.`;
 
         core.info(`Hello, ${owner}!!!`);
+        core.info(`repo, ${repo}!!!`);
+        core.info(`pull_number, ${pull_number}!!!`);
+        core.info(`token, ${token}!!!`);
 
-        // const octokit = new github.getOctokit(token);
+        const octokit = new github.getOctokit(token);
 
         // await octokit.rest.issues.createComment({
         //     owner,
