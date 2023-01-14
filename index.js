bd3c8c7b9904abb7d46da2e81e55d03afef09fe5
Линкевич Иван Владимирович <ilinkevich@alfaleasing.ru>
2023-01-14 15:07:09 +0300

    commit 4

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 // const report = require('../../cypress/report/report.json');
 
 const main = async () => {
     try {
         const owner = core.getInput('owner', { required: true });
-        const repo = core.getInput('repo', { required: true });
-        const pull_number = core.getInput('pull_number', { required: true });
-        const token = core.getInput('token', { required: true });
-        const url = 'https://training.cleverland.by/pull-request/merged'; // /pull-request/opened
-        const minimum_required_result = 80;
-        const tests_pass_percent = 50;
-        // const total_tests = report.stats.tests;
-        // const failures_test = report.stats.failures;
-        const issue_number = 123;
-        const body = `Процент пройденных: ${tests_pass_percent}.`;
+        // const repo = core.getInput('repo', { required: true });
+        // const pull_number = core.getInput('pull_number', { required: true });
+        // const token = core.getInput('token', { required: true });
+        // const url = 'https://training.cleverland.by/pull-request/merged'; // /pull-request/opened
+        // const minimum_required_result = 80;
+        // const tests_pass_percent = 50;
+        // // const total_tests = report.stats.tests;
+        // // const failures_test = report.stats.failures;
+        // const issue_number = 123;
+        // const body = `Процент пройденных: ${tests_pass_percent}.`;
 
-        const octokit = new github.getOctokit(token);
+        core.info(`Hello, ${owner}!!!`);
 
-        await octokit.rest.issues.createComment({
-            owner,
-            repo,
-            issue_number,
-            body,
-        });
+        // const octokit = new github.getOctokit(token);
+
+        // await octokit.rest.issues.createComment({
+        //     owner,
+        //     repo,
+        //     issue_number,
+        //     body,
+        // });
 
         // const { html_url } = await octokit.rest.pulls.get({
         //     owner,
