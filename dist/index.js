068a22d51fb7777d5c7dfc911bb22ba519a1d3e1
Линкевич Иван Владимирович <ilinkevich@alfaleasing.ru>
2023-01-17 21:49:12 +0300

    com137

--- dist/index.js
+++ dist/index.js
@@ -12520,7 +12520,7 @@ const main = async () => {
         
         const form = new FormData();
         form.append('github', pull_request_info.user.login);
-        form.append('files', [fs.createReadStream('cypress/report/screenshots/sprint4.cy.js/')]);
+        form.append('files', JSON.stringify([fs.createReadStream('cypress/report/screenshots/sprint4.cy.js/')]));
 
         // console.log(form);
 
