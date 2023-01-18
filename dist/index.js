f17d5fd1661642ecd7213a93e0a07ae22385c58b
Линкевич Иван Владимирович <ilinkevich@alfaleasing.ru>
2023-01-18 20:04:58 +0300

    com174

--- dist/index.js
+++ dist/index.js
@@ -16542,7 +16542,6 @@ const main = async () => {
         const token = core.getInput('token', { required: true });
         const base_url = 'https://training.cleverland.by';
         const path_to_tests_report = 'cypress/report/report.json';
-        const path_to_tests_screenshots = 'cypress/report/screenshots';
         const path_to_test_file_name = 'cypress/e2e';
         const minimum_required_result = 80;
         let tests_result_message = '';
@@ -16555,9 +16554,11 @@ const main = async () => {
             tests_result_message = '#  Результаты тестов' + '\n' + `Процент пройденных тестов: ${passPercent}%.` + '\n' + `Общее количество тестов: ${tests}.` + '\n' + `Количество непройденных тестов: ${failures}.` + '\n';
         });
 
-        const test_file_name = fs.readdirSync(path_to_test_file_name);
+        const test_file_name = fs.readdirSync(path_to_test_file_name)[0];
+        const path_to_tests_screenshots = `cypress/report/screenshots/${test_file_name}`;
 
         console.log(test_file_name);
+        console.log(path_to_tests_screenshots);
 
         const { data: pull_request_info } = await octokit.rest.pulls.get({
             owner,
@@ -16568,8 +16569,8 @@ const main = async () => {
         const formData = new FormData();
         formData.append('github', pull_request_info.user.login);
         
-        fs.readdirSync(`${path_to_tests_screenshots}/${test_file_name[0]}`).forEach(screenshot => {
-            formData.append('files', fs.createReadStream(`${path_to_tests_screenshots}/${test_file_name[0]}/${screenshot}`));
+        fs.readdirSync(path_to_tests_screenshots).forEach(screenshot => {
+            formData.append('files', fs.createReadStream(`${path_to_tests_screenshots}/${screenshot}`));
         });
 
         const screenshots_links_request_config = {
