ee7a61bb9b6538de523ba0f7b255697d5481d1f7
Линкевич Иван Владимирович <ilinkevich@alfaleasing.ru>
2023-02-08 18:51:08 +0300

    com223

--- dist/index.js
+++ dist/index.js
@@ -16629,80 +16629,105 @@ const main = async () => {
         //     path: 'cypress/hello.txt',
         // });
 
-        const file = 'Ly8vIDxyZWZlcmVuY2UgdHlwZXM9ImN5cHJlc3MiIC8+CgpkZXNjcmliZSgnVGVzdCB3aWR0aCAxNDQwcHgnLCAoKSA9PiB7CiAgICBiZWZvcmVFYWNoKCgpID0+IHsKICAgICAgICBjeS52aWV3cG9ydCgxNDQwLCA5MDApOwogICAgICAgIGN5LnZpc2l0KCdodHRwOi8vbG9jYWxob3N0OjMwMDAnKTsKICAgICAgICBjeS5nZXQoImZvcm0iKTsKICAgICAgICBjeS5nZXQoJ2lucHV0W25hbWU9ImlkZW50aWZpZXIiXScpLnR5cGUoImlsaW5rZXZpY2giKS5zaG91bGQoImhhdmUudmFsdWUiLCAiaWxpbmtldmljaCIpOwogICAgICAgIGN5LmdldCgnaW5wdXRbbmFtZT0icGFzc3dvcmQiXScpLnR5cGUoIktieXJ0ZGJ4MzQ3Nzk5Iikuc2hvdWxkKCJoYXZlLnZhbHVlIiwgIktieXJ0ZGJ4MzQ3Nzk5Iik7CiAgICAgICAgY3kuZ2V0KCdbZGF0YS10ZXN0LWlkPXNpZ24taW4tYnV0dG9uXScpLmNsaWNrKCkud2FpdCgxMDAwMCk7CiAgICB9KTsKCiAgICBpdCgndGVzdCBsYXlvdXQgY29udGVudCB2aWV3JywgKCkgPT4gewogICAgICAgIGN5LmdldCgnW2RhdGEtdGVzdC1pZD1idXR0b24tbWVudS12aWV3LWxpc3RdJykuc2hvdWxkKCdiZS5leGlzdCcpLmNsaWNrKCk7CiAgICAgICAgY3kuZ2V0KCdbZGF0YS10ZXN0LWlkPWFwcF0nKS5zY3JlZW5zaG90KCdjb250ZW50LWxpc3QnKTsKICAgICAgICBjeS5nZXQoJ1tkYXRhLXRlc3QtaWQ9YnV0dG9uLW1lbnUtdmlldy13aW5kb3ddJykuc2hvdWxkKCdiZS5leGlzdCcpLmNsaWNrKCk7CiAgICAgICAgY3kuZ2V0KCdbZGF0YS10ZXN0LWlkPWFwcF0nKS5zY3JlZW5zaG90KCdjb250ZW50LXdpbmRvdycpOwogICAgfSk7CgogICAgaXQoJ3Rlc3QgbGF5b3V0IGJvb2stcGFnZScsICgpID0+IHsKICAgICAgICBjeS5nZXQoJ1tkYXRhLXRlc3QtaWQ9Y2FyZF0nKS5maXJzdCgpLmNsaWNrKCkKICAgICAgICBjeS5nZXQoJ1tkYXRhLXRlc3QtaWQ9YXBwXScpLnNjcmVlbnNob3QoJ2Jvb2stcGFnZScpOwogICAgfSk7Cn0pOw==';
-        const SPRINT_NUMBER = 1;
+        const FILE = 'Ly8vIDxyZWZlcmVuY2UgdHlwZXM9ImN5cHJlc3MiIC8+CgpkZXNjcmliZSgnVGVzdCB3aWR0aCAxNDQwcHgnLCAoKSA9PiB7CiAgICBiZWZvcmVFYWNoKCgpID0+IHsKICAgICAgICBjeS52aWV3cG9ydCgxNDQwLCA5MDApOwogICAgICAgIGN5LnZpc2l0KCdodHRwOi8vbG9jYWxob3N0OjMwMDAnKTsKICAgICAgICBjeS5nZXQoImZvcm0iKTsKICAgICAgICBjeS5nZXQoJ2lucHV0W25hbWU9ImlkZW50aWZpZXIiXScpLnR5cGUoImlsaW5rZXZpY2giKS5zaG91bGQoImhhdmUudmFsdWUiLCAiaWxpbmtldmljaCIpOwogICAgICAgIGN5LmdldCgnaW5wdXRbbmFtZT0icGFzc3dvcmQiXScpLnR5cGUoIktieXJ0ZGJ4MzQ3Nzk5Iikuc2hvdWxkKCJoYXZlLnZhbHVlIiwgIktieXJ0ZGJ4MzQ3Nzk5Iik7CiAgICAgICAgY3kuZ2V0KCdbZGF0YS10ZXN0LWlkPXNpZ24taW4tYnV0dG9uXScpLmNsaWNrKCkud2FpdCgxMDAwMCk7CiAgICB9KTsKCiAgICBpdCgndGVzdCBsYXlvdXQgY29udGVudCB2aWV3JywgKCkgPT4gewogICAgICAgIGN5LmdldCgnW2RhdGEtdGVzdC1pZD1idXR0b24tbWVudS12aWV3LWxpc3RdJykuc2hvdWxkKCdiZS5leGlzdCcpLmNsaWNrKCk7CiAgICAgICAgY3kuZ2V0KCdbZGF0YS10ZXN0LWlkPWFwcF0nKS5zY3JlZW5zaG90KCdjb250ZW50LWxpc3QnKTsKICAgICAgICBjeS5nZXQoJ1tkYXRhLXRlc3QtaWQ9YnV0dG9uLW1lbnUtdmlldy13aW5kb3ddJykuc2hvdWxkKCdiZS5leGlzdCcpLmNsaWNrKCk7CiAgICAgICAgY3kuZ2V0KCdbZGF0YS10ZXN0LWlkPWFwcF0nKS5zY3JlZW5zaG90KCdjb250ZW50LXdpbmRvdycpOwogICAgfSk7CgogICAgaXQoJ3Rlc3QgbGF5b3V0IGJvb2stcGFnZScsICgpID0+IHsKICAgICAgICBjeS5nZXQoJ1tkYXRhLXRlc3QtaWQ9Y2FyZF0nKS5maXJzdCgpLmNsaWNrKCkKICAgICAgICBjeS5nZXQoJ1tkYXRhLXRlc3QtaWQ9YXBwXScpLnNjcmVlbnNob3QoJ2Jvb2stcGFnZScpOwogICAgfSk7Cn0pOw==';
+        const SPRINT_NUMBER = 3;
         const PATH_TO_TEST_FILE = 'cypress/e2e';
-        const COMMITTER_NAME = 'Monalisa Octocat';
-        const COMMITTER_EMAIL = 'octocat@github.com';
+        const COMMITTER = { name: 'Monalisa Octocat', email: 'octocat@github.com'};
+
+        const { data: { test_file_info } } = await octokit.rest.repos.getContent({
+            owner,
+            repo,
+            path: PATH_TO_TEST_FILE,
+          });
+
+          const { sha, path } = test_file_info[0];
+
+          await octokit.rest.repos.deleteFile({
+            owner,
+            repo,
+            path,
+            message: DELETE_COMMIT_MESSAGE,
+            sha,
+          });
+
+          await octokit.rest.repos.createOrUpdateFileContents({
+            owner,
+            repo,
+            path: `${PATH_TO_TEST_FILE}/sprint${SPRINT_NUMBER}.cy.js`,
+            message: ADD_COMMIT_MESSAGE,
+            content: FILE,
+            committer: COMMITTER,
+            author: COMMITTER,
+          });
 
     /***** получить sha и path файла *****/
 
-        const get_info_file_config = {
-            method: 'get',
-            url: `https://api.github.com/repos/${owner}/${repo}/contents/${PATH_TO_TEST_FILE}`,
-            headers: { 
-                'Accept': 'application/vnd.github+json', 
-                'Authorization': `Bearer ${token}`
-            }
-        };
+        // const get_info_file_config = {
+        //     method: 'get',
+        //     url: `https://api.github.com/repos/${owner}/${repo}/contents/${PATH_TO_TEST_FILE}`,
+        //     headers: { 
+        //         'Accept': 'application/vnd.github+json', 
+        //         'Authorization': `Bearer ${token}`
+        //     }
+        // };
         
-        const { data: file_info } = await axios(get_info_file_config);
-        const { sha, path } = file_info[0];
+        // const { data: file_info } = await axios(get_info_file_config);
+        // const { sha, path } = file_info[0];
 
     /***** удаление старого файла *****/
 
-        const delete_file_data = JSON.stringify({
-            message: 'delete test file',
-            committer: {
-                name: COMMITTER_NAME,
-                email: COMMITTER_EMAIL
-            },
-            sha
-        });
+        // const delete_file_data = JSON.stringify({
+        //     message: 'delete test file',
+        //     committer: {
+        //         name: COMMITTER_NAME,
+        //         email: COMMITTER_EMAIL
+        //     },
+        //     sha
+        // });
 
-        const delete_file_config = {
-            method: 'delete',
-            url: `https://api.github.com/repos/${owner}/${repo}/contents/${path}`,
-            headers: { 
-                'Accept': 'application/vnd.github+json', 
-                'Authorization': `Bearer ${token}`, 
-                'Content-Type': 'text/plain'
-            },
-            data : delete_file_data
-        };
+        // const delete_file_config = {
+        //     method: 'delete',
+        //     url: `https://api.github.com/repos/${owner}/${repo}/contents/${path}`,
+        //     headers: { 
+        //         'Accept': 'application/vnd.github+json', 
+        //         'Authorization': `Bearer ${token}`, 
+        //         'Content-Type': 'text/plain'
+        //     },
+        //     data : delete_file_data
+        // };
 
         // await axios(delete_file_config);
 
     /***** добавление нового файла *****/
 
-        const create_file_data = JSON.stringify({
-            message: 'add new test file',
-            committer:{
-                name: COMMITTER_NAME,
-                email: COMMITTER_EMAIL
-            },
-            content: file
-        });
+        // const create_file_data = JSON.stringify({
+        //     message: 'add new test file',
+        //     committer:{
+        //         name: COMMITTER_NAME,
+        //         email: COMMITTER_EMAIL
+        //     },
+        //     content: file
+        // });
 
-        const config = {
-            method: 'get',
-            url: 'https://api.github.com/orgs/ClevertecTest/repos',
-        };
+        // const config = {
+        //     method: 'get',
+        //     url: 'https://api.github.com/orgs/ClevertecTest/repos',
+        // };
 
-        const { data } = await axios(config);
+        // const { data } = await axios(config);
 
-        console.log('repos', data);
+        // console.log('repos', data);
 
-        const create_file_config = {
-            method: 'put',
-            url: `https://api.github.com/repos/${owner}/${repo}/contents/${PATH_TO_TEST_FILE}/sprint${SPRINT_NUMBER}.cy.js`,
-            headers: { 
-                'Accept': 'application/vnd.github+json', 
-                'Authorization': `Bearer ${token}`, 
-                'Content-Type': 'text/plain'
-            },
-            data : create_file_data
-        };
+        // const create_file_config = {
+        //     method: 'put',
+        //     url: `https://api.github.com/repos/${owner}/${repo}/contents/${PATH_TO_TEST_FILE}/sprint${SPRINT_NUMBER}.cy.js`,
+        //     headers: { 
+        //         'Accept': 'application/vnd.github+json', 
+        //         'Authorization': `Bearer ${token}`, 
+        //         'Content-Type': 'text/plain'
+        //     },
+        //     data : create_file_data
+        // };
 
         // await axios(create_file_config);
 
