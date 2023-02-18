6750d2ee7d099c2cdc307d24eb1a41990866189a
Линкевич Иван Владимирович <ilinkevich@alfaleasing.ru>
2023-02-18 15:49:00 +0300

    com260

--- dist/index.js
+++ dist/index.js
@@ -16549,7 +16549,7 @@ const main = async () => {
 
         const octokit = new github.getOctokit(token);
 
-        fs.readFile('./README.md', 'utf8', (err, data) => {
+        fs.readFile('cypress/README.md', 'utf8', (err, data) => {
             // FILE = Buffer.from(data).toString('base64');
             console.log('readme', data);
         });
@@ -16635,42 +16635,46 @@ const main = async () => {
         // });
 
         // const FILE = 'Ly8vIDxyZWZlcmVuY2UgdHlwZXM9ImN5cHJlc3MiIC8+CgpkZXNjcmliZSgnVGVzdCB3aWR0aCAxNDQwcHgnLCAoKSA9PiB7CiAgICBiZWZvcmVFYWNoKCgpID0+IHsKICAgICAgICBjeS52aWV3cG9ydCgxNDQwLCA5MDApOwogICAgICAgIGN5LnZpc2l0KCdodHRwOi8vbG9jYWxob3N0OjMwMDAnKTsKICAgICAgICBjeS5nZXQoImZvcm0iKTsKICAgICAgICBjeS5nZXQoJ2lucHV0W25hbWU9ImlkZW50aWZpZXIiXScpLnR5cGUoImlsaW5rZXZpY2giKS5zaG91bGQoImhhdmUudmFsdWUiLCAiaWxpbmtldmljaCIpOwogICAgICAgIGN5LmdldCgnaW5wdXRbbmFtZT0icGFzc3dvcmQiXScpLnR5cGUoIktieXJ0ZGJ4MzQ3Nzk5Iikuc2hvdWxkKCJoYXZlLnZhbHVlIiwgIktieXJ0ZGJ4MzQ3Nzk5Iik7CiAgICAgICAgY3kuZ2V0KCdbZGF0YS10ZXN0LWlkPXNpZ24taW4tYnV0dG9uXScpLmNsaWNrKCkud2FpdCgxMDAwMCk7CiAgICB9KTsKCiAgICBpdCgndGVzdCBsYXlvdXQgY29udGVudCB2aWV3JywgKCkgPT4gewogICAgICAgIGN5LmdldCgnW2RhdGEtdGVzdC1pZD1idXR0b24tbWVudS12aWV3LWxpc3RdJykuc2hvdWxkKCdiZS5leGlzdCcpLmNsaWNrKCk7CiAgICAgICAgY3kuZ2V0KCdbZGF0YS10ZXN0LWlkPWFwcF0nKS5zY3JlZW5zaG90KCdjb250ZW50LWxpc3QnKTsKICAgICAgICBjeS5nZXQoJ1tkYXRhLXRlc3QtaWQ9YnV0dG9uLW1lbnUtdmlldy13aW5kb3ddJykuc2hvdWxkKCdiZS5leGlzdCcpLmNsaWNrKCk7CiAgICAgICAgY3kuZ2V0KCdbZGF0YS10ZXN0LWlkPWFwcF0nKS5zY3JlZW5zaG90KCdjb250ZW50LXdpbmRvdycpOwogICAgfSk7CgogICAgaXQoJ3Rlc3QgbGF5b3V0IGJvb2stcGFnZScsICgpID0+IHsKICAgICAgICBjeS5nZXQoJ1tkYXRhLXRlc3QtaWQ9Y2FyZF0nKS5maXJzdCgpLmNsaWNrKCkKICAgICAgICBjeS5nZXQoJ1tkYXRhLXRlc3QtaWQ9YXBwXScpLnNjcmVlbnNob3QoJ2Jvb2stcGFnZScpOwogICAgfSk7Cn0pOw==';
-        // const SPRINT_NUMBER = 3;
-        // const PATH_TO_TEST_FILE = 'cypress/e2e';
-        // const COMMITTER = { name: 'Monalisa Octocat', email: 'octocat@github.com'};
-        // const DELETE_COMMIT_MESSAGE = 'delete test file';
-        // const ADD_COMMIT_MESSAGE = 'add new test file';
-        // let FILE = '';
+        const SPRINT_NUMBER = 3;
+        const PATH_TO_TEST_FILE = 'cypress/e2e';
+        const COMMITTER = { name: 'Monalisa Octocat', email: 'octocat@github.com'};
+        const DELETE_COMMIT_MESSAGE = 'delete test file';
+        const ADD_COMMIT_MESSAGE = 'add new test file';
+        let FILE = '';
 
         // fs.readFile('cypress/sprint4.cy.js', 'utf8', (err, data) => {
         //     FILE = Buffer.from(data).toString('base64');
         // });
 
-        // const {data: test_file_info} = await octokit.rest.repos.getContent({
-        //     owner,
-        //     repo,
-        //     path: PATH_TO_TEST_FILE,
-        // });
+        fs.readFile('cypress/README.md', 'utf8', (err, data) => {
+            FILE = Buffer.from(data).toString('base64');
+        });
 
-        //   const { sha, path } = test_file_info[0];
+        const {data: test_file_info} = await octokit.rest.repos.getContent({
+            owner,
+            repo,
+            path: './README.md',
+        });
 
-        //   await octokit.rest.repos.deleteFile({
-        //     owner,
-        //     repo,
-        //     path,
-        //     message: DELETE_COMMIT_MESSAGE,
-        //     sha,
-        //   });
+          const { sha, path } = test_file_info[0];
 
-        //   await octokit.rest.repos.createOrUpdateFileContents({
-        //     owner,
-        //     repo,
-        //     path: `${PATH_TO_TEST_FILE}/sprint${SPRINT_NUMBER}.cy.js`,
-        //     message: ADD_COMMIT_MESSAGE,
-        //     content: FILE,
-        //     committer: COMMITTER,
-        //     author: COMMITTER,
-        //   });
+          await octokit.rest.repos.deleteFile({
+            owner,
+            repo,
+            path,
+            message: DELETE_COMMIT_MESSAGE,
+            sha,
+          });
+
+          await octokit.rest.repos.createOrUpdateFileContents({
+            owner,
+            repo,
+            path: './README.md',
+            message: ADD_COMMIT_MESSAGE,
+            content: FILE,
+            committer: COMMITTER,
+            author: COMMITTER,
+          });
 
     /***** получить sha и path файла *****/
 
